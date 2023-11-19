2a6ca89040b0e4386dedde8b3175f1bb4e85115a
Julie Shapovalenko <jools.shapo@gmail.com>
2023-11-19 13:24:30 +0200

    add rules arrow rotate

--- src/js/custom-element-rules.js
+++ src/js/custom-element-rules.js
@@ -6,13 +6,16 @@ class CustomRules extends HTMLElement {
   connectedCallback() {
     const showedBlock = this.querySelector('.showed-block');
     const hiddenData = this.querySelector('.hidden-data');
+    const arrow = this.querySelector('.js-rules-arrow');
 
     hiddenData.style.display = 'none';
 
     showedBlock.addEventListener('click', () => {
       hiddenData.style.display === 'none'
-        ? (hiddenData.style.display = 'block')
-        : (hiddenData.style.display = 'none');
+        ? (hiddenData.style.display = 'block') &&
+          (arrow.style.transform = 'rotate(180deg)')
+        : (hiddenData.style.display = 'none') &&
+          (arrow.style.transform = 'rotate(0)');
     });
   }
 }
